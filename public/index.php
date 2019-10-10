e1590494d98408d766f1ef24a2417ba957cb2024
Matt <m.geslin974@gmail.com>
2019-10-10 13:15:34 +0200

    login inputs added - redirect to blog content with posts

--- public/index.php
+++ public/index.php
@@ -1,2 +1,14 @@
-<?php
+<?php $title = "Connexion au site"; ?>
 
+<?php ob_start(); ?>
+    <header>
+        <h1>Bienvenue sur mon blog !</h1>
+        <form action="../src/View/postsview.php" method="post">
+            <label for="login">Veuillez choisir un nom d'utilisateur :</label>
+            <input type="password" id="login" name="login">
+            <input type="submit" value="Valider">
+        </form>
+    </header>
+<?php $content = ob_get_clean(); ?>
+
+<?php require('../src/View/template.php');
