583bc6f6d79e7d3ffa2e8f07188390ecc1f131e5
Matt <m.geslin974@gmail.com>
2019-10-28 09:42:15 +0100

    CommentManager Created - sql comments

---
+++ src/Model/CommentManager.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Model;
+
+use App\Model\Factory\PDOFactory;
+
+class CommentManager extends PDOFactory
+{
+    public function getComments($postId)
+    {
+        $db = $this->getPDO();
+        $comments = $db->prepare('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE post_id = ? ORDER BY comment_date DESC');
+        $comments->execute(array($postId));
+
+        return $comments;
+    }
+
+    public function postComment($postId, $author, $comment)
+    {
+        $db = $this->getPDO();
+        $comments = $db->prepare('INSERT INTO comments(post_id, author, comment, comment_date) VALUES(?, ?, ?, NOW())');
+        $affectedLines = $comments->execute(array($postId, $author, $comment));
+
+        return $affectedLines;
+    }
+}
