858831ca3f0e7ad4c581b969a010a8b054a9314f
Matt <m.geslin974@gmail.com>
2019-11-03 16:20:38 +0100

    Require link issue fixed

--- src/Model/Factory/PDOFactory.php
+++ src/Model/Factory/PDOFactory.php
@@ -19,7 +19,7 @@ class PDOFactory
      */
     public static function getPDO()
     {
-        require_once 'config/db.php';
+        require_once '../config/db.php';
         if (self::$pdo === null) {
             self::$pdo = new PDO(DB_DSN, DB_USER, DB_PASS, DB_OPTIONS);
             self::$pdo->exec('SET NAMES UTF8');
