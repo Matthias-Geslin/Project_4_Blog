016a0154b5712813c29102c66d08096f1bbf8355
Matt <m.geslin974@gmail.com>
2019-11-11 01:33:37 +0100

    ConnexionController view Created

---
+++ src/Controller/ConnexionController.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Controller;
+use App\Model\Factory\ModelFactory;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+/**
+ * Class ConnexionController
+ * @package App\Controller
+ */
+class ConnexionController extends MainController
+{
+    /**
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function defaultMethod()
+    {
+        /* Return the Rendering of the View home.twig */
+        return $this->render('connexion.twig');
+    }
+}
