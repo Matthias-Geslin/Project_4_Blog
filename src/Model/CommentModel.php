4c5e995f8945e17bc868c8f34e28c50498e9c201
Matt <m.geslin974@gmail.com>
2019-11-19 22:20:14 +0100

    Comment model update - refactoring & comments

--- src/Model/CommentModel.php
+++ src/Model/CommentModel.php
@@ -6,19 +6,27 @@ use App\Model\Factory\PDOFactory;
 
 class CommentModel extends PDOFactory
 {
+    /**
+     * @param $postId
+     * @return bool|\PDOStatement
+     */
     public function getComments($postId)
     {
-        $db = $this->getPDO();
-        $comments = $db->prepare('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE post_id = ? ORDER BY comment_date DESC');
+        $comments = $this->getPDO()->prepare('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE post_id = ? ORDER BY comment_date DESC');
         $comments->execute(array($postId));
 
         return $comments;
     }
 
+    /**
+     * @param $postId
+     * @param $author
+     * @param $comment
+     * @return bool
+     */
     public function postComment($postId, $author, $comment)
     {
-        $db = $this->getPDO();
-        $comments = $db->prepare('INSERT INTO comments(post_id, author, comment, comment_date) VALUES(?, ?, ?, NOW())');
+        $comments = $this->getPDO()->prepare('INSERT INTO comments(post_id, author, comment, comment_date) VALUES(?, ?, ?, NOW())');
         $affectedLines = $comments->execute(array($postId, $author, $comment));
 
         return $affectedLines;
