02473f06dcda45db444481d003c90f44c3fca510
Matt <m.geslin974@gmail.com>
2019-11-19 22:14:53 +0100

    Renamed default method & edit comment controller

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controller;
 
+use App\Model\CommentModel;
 use App\Model\Factory\ModelFactory;
 use Twig\Error\LoaderError;
 use Twig\Error\RuntimeError;
@@ -8,5 +9,46 @@ use Twig\Error\SyntaxError;
 
 class CommentController extends MainController
 {
+    /**
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function launchMethod()
+    {
+        $postsList = ModelFactory::getModel('posts')->listData();
+
+        return $this->render("post.twig", ['posts' => $postsList]);
+    }
+
+    /**
+     * @return string
+     * @throws
+     * @throws
+     * @throws
+     */
+    public function deleteComment() {
+
+    }
+
+    /**
+     * @param $postId
+     * @param $author
+     * @param $comment
+     */
+    function addComment($postId, $author, $comment)
+    {
+        $commentManager = new CommentModel;
+
+        $affectedLines = $commentManager->postComment($postId, $author, $comment);
+
+        if ($affectedLines === false) {
+            throw new Exception('Impossible d\'ajouter le commentaire !');
+        }
+        else {
+            header('Location: index.php?action=post&id=' . $postId);
+        }
+    }
 
 }
