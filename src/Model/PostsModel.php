d2f6d21b9dac961dbd0861787b9465694041d771
Matt <m.geslin974@gmail.com>
2019-11-25 16:45:22 +0100

    PostModel removed & all functions moved into PostsModel

--- src/Model/PostsModel.php
+++ src/Model/PostsModel.php
@@ -1,9 +1,43 @@
 <?php
+
 namespace App\Model;
 
-/**
- * Class PostsModel
- * Manages Post Data
- * @package App\Model
- */
-class PostsModel extends MainModel {}
+use App\Model\Factory\PDOFactory;
+
+class PostModel
+{
+    /**
+     * @return false|\PDOStatement
+     */
+    public function getAllPosts()
+    {
+        $req = PDOFactory::getPDO()->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0,5');
+//        $req = $this->getPDO()->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0,5');
+
+        return $req;
+    }
+
+    /**
+     * @param $postId
+     * @return mixed
+     */
+    public function getPost($postId)
+    {
+        $req = PDOFactory::getPDO()->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts WHERE id = ?');
+//        $req = $this->getPDO()->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts WHERE id = ?');
+        $req->execute(array($postId));
+
+        return $req;
+    }
+
+    /**
+     * @return false|\PDOStatement
+     */
+    public function getLastPost()
+    {
+        $req = PDOFactory::getPDO()->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 1');
+//        $req = $this->getPDO()->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 1');
+
+        return $req;
+    }
+}
