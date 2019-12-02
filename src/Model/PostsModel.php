e879929d764992b2c2de33d260e839577c2a87f3
Matt <m.geslin974@gmail.com>
2019-12-02 01:36:41 +0100

    PostsModel update - removed duplicate functions as list & create Data

--- src/Model/PostsModel.php
+++ src/Model/PostsModel.php
@@ -4,19 +4,8 @@ namespace App\Model;
 
 use App\Model\Factory\PDOFactory;
 
-class PostModel
+class PostsModel extends MainModel
 {
-    /**
-     * @return false|\PDOStatement
-     */
-    public function getAllPosts()
-    {
-        $req = PDOFactory::getPDO()->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0,5');
-//        $req = $this->getPDO()->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0,5');
-
-        return $req;
-    }
-
     /**
      * @param $postId
      * @return mixed
@@ -24,7 +13,6 @@ class PostModel
     public function getPost($postId)
     {
         $req = PDOFactory::getPDO()->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts WHERE id = ?');
-//        $req = $this->getPDO()->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts WHERE id = ?');
         $req->execute(array($postId));
 
         return $req;
@@ -35,9 +23,8 @@ class PostModel
      */
     public function getLastPost()
     {
-        $req = PDOFactory::getPDO()->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 1');
-//        $req = $this->getPDO()->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 1');
-
+        $req = PDOFactory::getPDO()->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 1');
+        $req->execute();
         return $req;
     }
 }
