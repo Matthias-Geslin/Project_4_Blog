4dbb5ed9c3d8db5bbea6ff045b44d61155429883
Matt <m.geslin974@gmail.com>
2019-12-18 01:11:05 +0100

    admin controller update - added creation, edit & delete method for users

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -20,14 +20,85 @@ class AdminController extends MainController
      */
     public function launchMethod()
     {
-        if ($this->session->islogged())
-        {
-          $posts = ModelFactory::getModel('posts')->listData();
+      $posts = ModelFactory::getModel('posts')->listData();
+      $admin = ModelFactory::getModel('admin')->listData();
 
-          return $this->render("backend/admin.twig", [
-              'posts' => $posts
-          ]);
+      return $this->render("backend/admin.twig", [
+          'posts' => $posts,
+          'admin' => $admin
+      ]);
+    }
+
+    /**
+     *  @return array
+     */
+    private $post_content = [];
+
+    /**
+     * @return string
+     */
+    private function postData()
+    {
+      $this->post_content['first_name']  = $this->post['first_name'];
+      $this->post_content['last_name']   = $this->post['last_name'];
+      $this->post_content['email']       = $this->post['email'];
+      $this->post_content['pass']        = $this->post['pass'];
+      $this->post_content['status']      = $this->post['status'];
+    }
+
+    /**
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function createMethod()
+    {
+      $first_name   = $this->post['first_name'];
+      $last_name    = $this->post['last_name'];
+      $email        = $this->post['email'];
+      $pass         = $this->post['pass'];
+      $status       = $this->post['status'];
+
+        if (empty($first_name && $last_name && $email && $pass && $status)) {
+            return $this->render('backend/adminCreate.twig');
+        } else {
+            $createdUser = ModelFactory::getModel('admin')->createData([
+                'first_name'  => $first_name,
+                'last_name'   => $last_name,
+                'email'       => $email,
+                'pass'        => $pass,
+                'status'      => $status
+            ]);
+           $this->redirect('admin', ['createdUser' => $createdUser]);
         }
-        $this->redirect('connexion');
+    }
+
+    /**
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function deleteMethod()
+    {
+       ModelFactory::getModel('admin')->deleteData($this->get['id']);
+
+       $this->redirect('admin');
+    }
+
+    public function modifyMethod()
+    {
+      if (!empty($this->post)) {
+        $this->postData();
+
+        ModelFactory::getModel('admin')->updateData($this->get['id'], $this->post_content);
+
+        $this->redirect('admin');
+      }
+      $admin = ModelFactory::getModel('admin')->readData($this->get['id']);
+
+      return $this->render('backend/adminModify.twig', [
+        'admin' => $admin
+      ]);
     }
 }
